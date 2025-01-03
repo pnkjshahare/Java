437409c3420e57eb7924b87a10b806cf5d3ca12a
Pankajshahare <pankajsahare.04@gmail.com>
2025-01-03 17:07:57 +0530

    java

--- Basic/Main.java
+++ Basic/Main.java
@@ -1,5 +1,10 @@
 class Main{
     public static void main(String args[]){
-        System.out.println("pankaj");
+        // System.out.println("pankaj");
+        int value1=10;
+        byte value2=(byte)value1;
+        System.out.println(value2);
+        boolean value3=true;
+        System.out.println(value3);
     }
 }
---
+++ Basic/addition.java
@@ -0,0 +1,38 @@
+
+class addition {
+
+    public static void main(String[] args) {
+        // int val1 = 30;
+        // int val2 = 20;
+        // int result = val1 + val2;
+        // System.out.println(result);
+        // if (val1 == val2) {
+        //     System.out.println("val1 is greater");
+        // } else {
+        //     System.out.println("val2 is greater");
+        // }
+
+        int a = 53;
+        int b = 30;
+        int c = 40;
+        // if (val1 > val2 && a < b) {
+        //     System.err.println("true");
+        // } else {
+        //     System.err.println("false");
+        // }
+        if (a > b && a > c) {
+            System.out.println(" A is Greater :" + a);
+        } else if (b > a && b > c) {
+            System.out.println("B is greater :" + b);
+        } else {
+            System.out.println("C is greater :" + c);
+        }
+        // int result = a % 2 == 0 ? 10 : 20;
+        // System.out.println(result);
+    }
+}
+//1. Arithematic operator => +,- ,*,/,%
+//2.relational operator => <,>,>=,<=,!=
+//3. logical operator =>&&,||,^(ans,or,xor)
+//4. Conditional Statement => if,if else, if else if
+//5. Ternary Operator =>
