188b56849021cff7973ef268336e8424784cd9de
Pankajshahare <pankajsahare.04@gmail.com>
2025-01-03 20:49:07 +0530

    java

--- Basic/addition.java
+++ Basic/addition.java
@@ -12,23 +12,72 @@ class addition {
         //     System.out.println("val2 is greater");
         // }
 
-        int a = 53;
-        int b = 30;
-        int c = 40;
+        // int a = 53;
+        // int b = 30;
+        // int c = 40;
         // if (val1 > val2 && a < b) {
         //     System.err.println("true");
         // } else {
         //     System.err.println("false");
         // }
-        if (a > b && a > c) {
-            System.out.println(" A is Greater :" + a);
-        } else if (b > a && b > c) {
-            System.out.println("B is greater :" + b);
-        } else {
-            System.out.println("C is greater :" + c);
-        }
+        // if (a > b && a > c) {
+        //     System.out.println(" A is Greater :" + a);
+        // } else if (b > a && b > c) {
+        //     System.out.println("B is greater :" + b);
+        // } else {
+        //     System.out.println("C is greater :" + c);
+        // }
         // int result = a % 2 == 0 ? 10 : 20;
         // System.out.println(result);
+        //**************Switch Case Statement */
+        // int n = 1;
+        // switch (n) {
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("thursday");
+        //         break;
+        //     case 5:
+        //         System.out.println("frinday");
+        //         break;
+        //     case 6:
+        //         System.out.println("saturday");
+        //         break;
+        //     case 7:
+        //         System.out.println("Sunday");
+        //         break;
+        //     default:
+        //         System.out.println("Enter valid Day no");
+        // }
+        //********* while loops ******/
+        // int count = 1;
+        // while (count <= 10) {
+        //     System.out.println("hello " + count);
+        //     count++;
+        // }
+        // ********* do while
+        // do {
+        //     System.out.println("hello " + count);
+        //     count++;
+        // } while (count <= 5);
+        //************for loop */
+        // for (int i = 5; i >= 0; i--) {
+        //     System.out.println("hello pankaj " + i);
+        //     for (int j = 9; j <= 18; j++) {
+        //         System.out.println("hr " + j);
+        //     }
+        // }
+        // in java true menas not 1 or any other no. true is strictly boolean and 1,2,3 is strickly integer
+        if (true) {
+            System.out.println("pankaj");
+        }
     }
 }
 //1. Arithematic operator => +,- ,*,/,%
@@ -36,3 +85,5 @@ class addition {
 //3. logical operator =>&&,||,^(ans,or,xor)
 //4. Conditional Statement => if,if else, if else if
 //5. Ternary Operator =>
+//6.switch case
+//7.loops-> for loops, while loops, do while loops
